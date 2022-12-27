98dc59cf4fb5606a9502e834bc702c67b7a578c4
Gursewaksingh1 <76276827+Gursewaksingh1@users.noreply.github.com>
2022-12-27 11:10:15 +0530

    Update app.js
    a

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ app.use(express.json());
 app.use(cors());
 app.use("/",userRouter);
 app.use("/post",postRouter);
-
+app.use((err, req, res, next) => {
+    res.status(404).send({
+      status: "failed",
+      statusCode: 404,
+      error: "wrong URL please check your URL and http method",
+    });
+  });
 mongoose.connect(_URI)
   .then(result => {
      console.log(result)
